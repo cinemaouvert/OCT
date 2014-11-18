3fdd9753190115f3414d765c36da8224a93326be
ThibaudL <lamarchethibaud@hotmail.fr>
2014-11-18 14:54:23 +0200

    create dispatcher

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "View/MainWindow.h"
+#include "Controller/OCTDispatcher.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     View::MainWindow w;
+    Controller::OCTDispatcher dispatcher;
     w.show();
 
     return a.exec();
