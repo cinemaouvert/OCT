f2f69fe49fdef8345bc7c0178236c35a4dbcc2a8
romainboutin <romain.boutin@etu.univ-poitiers.fr>
2014-11-19 09:33:59 +0100

    model modification

--- Model/Audio.cpp
+++ Model/Audio.cpp
@@ -6,24 +6,33 @@ using namespace std;
 
 Model::Audio::Audio() {
     m_uID = "";
+    this->m_parameters = NULL;
 }
 
 Model::Audio::Audio(QString uID) {
     m_uID = uID;
+    this->m_parameters = NULL;
 }
 
 Model::Audio::Audio(const Audio& a) {
     m_uID = a.m_uID;
+    QMap<QString,Parameter*> *param(a.m_parameters) ;
+    this->m_parameters = param;
 }
 
 Model::Audio& Model::Audio::operator=(const Audio& a) {
     if ( this != &a ) {
         m_uID = a.m_uID;
+        QMap<QString,Parameter*> *param(a.m_parameters) ;
+        this->m_parameters = param;
     }
     return *this;
 }
 
-Model::Audio::~Audio() {}
+Model::Audio::~Audio() {
+    if (this->m_parameters != NULL)
+        delete(this->m_parameters);
+}
 
 ostream& Model::Audio::operator >> (ostream& o){
     throw "Not yet implemented";
