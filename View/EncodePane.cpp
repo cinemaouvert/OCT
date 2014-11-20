3b19317399301215aff6072abea6c664e0bf3724
William Le Coroller <william.le.coroller@gmail.com>
2014-11-20 15:29:45 +0100

    Added documentation within the View files.
    Added documentation within the View files.

--- View/EncodePane.cpp
+++ View/EncodePane.cpp
@@ -1,3 +1,30 @@
+/**********************************************************************************
+ * This file is part of Open Cinema Transcoder (OCT).
+ *
+ * Copyright (C) 2014 Catalogue Ouvert du Cinéma <dev@cinemaouvert.fr>
+ *
+ * Authors: Romain Boutin <romain.boutin@etu.univ-poitiers.fr>
+ *          Thibaud Lamarche <lamarchethibaud@hotmail.com>
+ *          William Le Coroller <william.le.coroller@gmail.com>
+ *          Denis Saunier <saunier.denis.86@gmail.com>
+ *
+ * Open Cinema Transcoder is an initiative of Catalogue Ouvert du Cinéma.
+ * The software was developed by four students of University of Poitiers
+ * as school project.
+ *
+ * Open Cinema Transcoder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Open Cinema Transcoder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Cinema Transcoder. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************************/
 
 #include "View/EncodePane.h"
 #include "View/MainWindow.h"
