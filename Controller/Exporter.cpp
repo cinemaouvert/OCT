6feab321518cb8f89069fa244d6a3ac1fc5f25b4
DSaunier <saunier.denis.86@gmail.com>
2014-12-16 11:22:17 +0100

    Edit qbytearray

--- Controller/Exporter.cpp
+++ Controller/Exporter.cpp
@@ -50,7 +50,7 @@ bool Controller::Exporter::sendInformationsToJSON(Model::Project* project)
     if(project != NULL){
         QList<Model::Information*> *informations = project->informations();
         if(informations != NULL){
-            QByteArray  json = "{\"key_user\":\"" + m_Database->userKey()+ "\",\"quick\":\"false\"";
+            QByteArray  json = "{";
             for (int i = 0; i < informations->size(); i++) {
                 Model::Information* info = informations->at(i);
                 json += "\"" + info->name() + "\":\"" + info->value() +"\",";
