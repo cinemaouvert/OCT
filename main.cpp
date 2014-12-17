ba965cb8cec4156602b8b0da3fdb1004b63cc598
ThibaudL <lamarchethibaud@hotmail.fr>
2014-12-17 09:25:12 +0100

    Changed style

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "Controller/OCTDispatcher.h"
 #include <QApplication>
-
+#include <QStyleFactory>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     Controller::OCTDispatcher dispatcher;
+
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
 
     return a.exec();
 }
