2ec4a499ec7959949987b5c5f4d05d337bf078ae
DSaunier <saunier.denis.86@gmail.com>
2015-01-29 16:59:48 +0100

    Add utils method crypto

--- src/Controller/Utils.cpp
+++ src/Controller/Utils.cpp
@@ -1,6 +1,8 @@
 #include "Utils.h"
 #include <QDebug>
-
+#include <QCryptographicHash>
+#include <QDialog>
+#include <QFile>
 
 QString Utils::convertFract(QString var)
 {
@@ -33,4 +35,23 @@ QString Utils::convertFract(QString var)
 QString Utils::bpsToKbps(QString var)
 {
     return QString::number(var.toInt()/1000);
+}
+
+QString Utils::md5File(QString pathFile){
+    QCryptographicHash md5(QCryptographicHash::Md5);
+    md5.addData(readAllFile(pathFile));
+    QByteArray hah = md5.result();
+    return hah.toHex();
+}
+QString Utils::sha1File(QString pathFile){
+    QCryptographicHash sha1(QCryptographicHash::Sha1);
+    sha1.addData(readAllFile(pathFile));
+    QByteArray hah = sha1.result();
+    return hah.toHex();
+}
+
+QByteArray Utils::readAllFile(QString pathFile){
+    QFile file(pathFile);
+    file.open(QIODevice::ReadOnly);
+    return file.readAll();
 }
--- src/Controller/Utils.h
+++ src/Controller/Utils.h
@@ -8,6 +8,10 @@ class Utils
 public:
     static QString convertFract(QString var);
     static QString bpsToKbps(QString var);
+    static QString sha1File(QString pathFile);
+    static QString md5File(QString pathFile);
+private:
+    static QByteArray readAllFile(QString pathFile);
 };
 
 #endif // UTILS_H
